e0e33244088ce046a362608323fcefb3c646232b
jsaique <jsaique11@gmail.com>
2023-09-27 12:50:48 -0400

    EmailJS functionality

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,72 +1,88 @@
 "use client";
 
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import emailjs from "@emailjs/browser";
 
 export default function ContactForm() {
-  const [username, setUsername] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [email, setEmail] = useState("");
-  const [subject, setSubject] = useState("");
-  const [message, setMessage] = useState("");
-  const [errMsg, setErrMsg] = useState("");
-  const [successMsg, setSuccessMsg] = useState("");
+  const initialValues = {
+    name: "",
+    phone: "",
+    email: "",
+    subject: "",
+    message: "",
+  };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [submitted, setSubmitted] = useState(false);
 
-  // Email Validation
-  const emailValidation = () => {
-    return String(email)
-      .toLocaleLowerCase()
-      .match(/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/);
+  //Getting the key and value of the form
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    //[name] will assign the name to the key when typing in that form "same for other forms"
+    setFormValues({ ...formValues, [name]: value });
+    // console.log(formValues);
   };
 
-  // Form and EmailJS
+  //Form
   const form = useRef();
 
+  //EmailJS
   const sendEmail = (e) => {
     e.preventDefault();
+    setFormErrors(validate(formValues));
+    setSubmitted(true);
 
-    emailjs
-      .sendForm(
-        process.env.EMAILJS_SERVICE_ID,
-        process.env.EMAILJS_TEMPLATE_ID,
-        form.current,
-        process.env.EMAILJS_PUBLIC_ID
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
-  // End of emailJS
-
-  const handleSend = (e) => {
-    e.preventDefault();
-    if (username === "") {
-      setErrMsg("Name is required!");
-    } else if (phoneNumber === "") {
-      setErrMsg("Phone number is required!");
-    } else if (email === "") {
-      setErrMsg("Email is required!");
-    } else if (!emailValidation(email)) {
-      setErrMsg("Give a valid email!");
-    } else if (subject === "") {
-      setErrMsg("Subject is required!");
-    } else if (message === "") {
-      setErrMsg("Message is required!");
-    } else {
-      setSuccessMsg(`Thank you ${username}, Your Message Has Been Sent!`);
-      setErrMsg("");
-      setUsername("");
-      setPhoneNumber("");
-      setEmail("");
-      setSubject("");
-      setMessage("");
-      console.log(username, phoneNumber, email, subject, message);
+    if (formValues.email && formValues.subject && formValues.message) {
+      emailjs
+        .sendForm(
+          "service_9fehgbc",
+          "template_771d0za",
+          form.current,
+          "DovzGJEX_b7hIpw4A"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+      form.current.reset();
     }
+  };
+
+  useEffect(() => {
+    // console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && submitted) {
+      // console.log(formValues);
+    }
+  }, [formErrors]);
+
+  //Form validation
+  const validate = (values) => {
+    const errors = {};
+    const regex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!values.name) {
+      errors.name = "* Name is required";
+    }
+    if (!values.phone) {
+      errors.phone = "Phone number is required";
+    }
+    if (!values.email) {
+      errors.email = "* Email is required";
+    }
+    if (!regex.test(values.email)) {
+      errors.email = "* Enter a valid email";
+    }
+    if (!values.subject) {
+      errors.subject = "* Subject is required";
+    }
+    if (!values.message) {
+      errors.message = "* Write a message";
+    }
+    return errors;
   };
 
   return (
@@ -77,77 +93,61 @@ export default function ContactForm() {
       <h1 className="text-2xl font-bold text-center p-4">
         Interested on renting?
       </h1>
-      <form ref={form} onSubmit={sendEmail} className="max-w-[600px] m-auto ">
-        {errMsg && (
-          <p className="py-3 text-center text-red-600 text-base tracking-wide animate-bounce">
-            *{errMsg}
-          </p>
-        )}
-        {successMsg && (
-          <p className="py-3 text-center text-cyan-700 text-base tracking-wide animate-bounce">
-            {successMsg}
-          </p>
-        )}
-        <div className="grid grid-cols-2 gap-2">
-          <input
-            onChange={(e) => setUsername(e.target.value)}
-            value={username}
-            className="border shadow-lg p-3 focus-visible:outline-cyan-700"
-            type="text"
-            placeholder="Name"
-            name="name"
-          />
-          <input
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            value={phoneNumber}
-            className="border shadow-lg p-3 focus-visible:outline-cyan-700"
-            type="text"
-            placeholder="Phone Number"
-            name="phone"
-          />
-        </div>
+      <form className="max-w-[600px] m-auto " ref={form} onSubmit={sendEmail}>
         <input
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
           className="border shadow-lg p-3 w-full my-4 focus-visible:outline-cyan-700"
+          onChange={handleChange}
+          type="text"
+          placeholder="Name"
+          name="name"
+        />
+        <p className="text-sm font-medium text-red-500">{formErrors.name}</p>
+        <input
+          className="border shadow-lg p-3 focus-visible:outline-cyan-700 w-full"
+          onChange={handleChange}
+          type="text"
+          placeholder="Phone Number"
+          name="phone"
+        />
+        <p className="text-sm font-medium text-red-500">{formErrors.phone}</p>
+        <input
+          className="border shadow-lg p-3 w-full my-4 focus-visible:outline-cyan-700"
+          onChange={handleChange}
           type="email"
           placeholder="Email"
           name="email"
         />
+        <p className="text-sm font-medium text-red-500">{formErrors.email}</p>
         <input
-          onChange={(e) => setSubject(e.target.value)}
-          value={subject}
           className="border shadow-lg p-3 w-full mb-4 focus-visible:outline-cyan-700"
+          onChange={handleChange}
           type="text"
           placeholder="Subject"
           name="subject"
         />
+        <p className="text-sm font-medium text-red-500">{formErrors.subject}</p>
         <textarea
-          onChange={(e) => setMessage(e.target.value)}
-          value={message}
           className="border shadow-lg p-3 w-full resize-none focus-visible:outline-cyan-700"
+          onChange={handleChange}
           cols="30"
           rows="10"
           placeholder="Message"
           name="message"
         ></textarea>
+        <p className="text-sm font-medium text-red-500">{formErrors.message}</p>
+        {Object.keys(formErrors).length === 0 && submitted ? (
+          <p className="text-lg font-semibold text-green-500 text-center">
+            {formValues.name} your message has been sent!
+          </p>
+        ) : (
+          ""
+        )}
         <button
           type="submit"
-          onClick={handleSend}
           className="border bg-cyan-700 text-slate-200 shadow-lg p-3 w-full mt-2 mb-3"
         >
           Send Message
         </button>
-        {errMsg && (
-          <p className="py-3 text-center text-red-600 text-base tracking-wide animate-bounce">
-            *{errMsg}
-          </p>
-        )}
-        {successMsg && (
-          <p className="py-3 text-center text-cyan-700 text-base tracking-wide animate-bounce">
-            {successMsg}
-          </p>
-        )}
       </form>
     </section>
   );
